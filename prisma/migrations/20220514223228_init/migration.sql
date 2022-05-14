043ca7ac43cadb40accbd2128f35d0ed77d5cf87
cesarmezab <99105052+CesarMezaB@users.noreply.github.com>
2022-05-14 15:35:37 -0700

    Agrengado tabla Animal generada de Prisma

---
+++ prisma/migrations/20220514223228_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Animal" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "description_danger" TEXT NOT NULL,
+    "img_Front_Path" VARCHAR(255) NOT NULL,
+    "img_back_Path" VARCHAR(255) NOT NULL,
+    "dateCreated" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "lastUpdated" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Animal_name_key" ON "Animal"("name");
