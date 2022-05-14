12c3ba15b4cec94a538d791456bd8589a8505920
Bruno Gomez <br1_gio@hotmail.com>
2022-05-14 14:40:00 -0500

    Front y back en un solo servidor
    [Novedad]: Ahora express sirve los estáticos de Vue, ya NO es necesario iniciar el servidor de Vue. :)

--- server.js
+++ server.js
@@ -1,39 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-app.get('/', (req, res) => {
-  res.json({message: 'alive'});
+app.get("/api/users", async (req, res) => {
+  const allUsers = await prisma.user.findMany({});
+  res.json(allUsers);
 });
 
-app.get('/users', async (req, res) => {
-    const allUsers =  await prisma.user.findMany({});
-    res.json(allUsers);
-  });
+app.get("/apí/users/:id", async (req, res) => {
+  const id = req.params.id;
+  const user = await prisma.user.findUnique({ where: { id: parseInt(id) } });
+  res.json(user);
+});
 
-  app.get('/users/:id', async (req, res) => {
-    const id = req.params.id;
-    const user = await prisma.user.findUnique({where: {id: parseInt(id)}});
-    res.json(user);
-  });
-
-  app.post('/users', async (req, res) => {
-    const user = {
-      name: req.body.name,
-      email: req.body.email,
-      animal_Specie: req.body.animal_Specie,
-      commentary: req.body.commentary
-
-     };
-    const message = 'Usuario creado creado.';
-    await prisma.user.create({data: user});
-    return res.json({message});
-  });
+app.post("/api/users", async (req, res) => {
+  const user = {
+    name: req.body.name,
+    email: req.body.email,
+    animal_Specie: req.body.animal_Specie,
+    commentary: req.body.commentary,
+  };
+  const message = "Usuario creado creado.";
+  await prisma.user.create({ data: user });
+  return res.json({ message });
+});
 
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
 });
+
+app.use(express.static(__dirname + "/client/dist"));
