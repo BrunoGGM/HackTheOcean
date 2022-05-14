07f1f89d932dcd6c6cc3bd90c4deb506c6d36bdd
cesarmezab <99105052+CesarMezaB@users.noreply.github.com>
2022-05-13 23:05:31 -0700

    Creando archivo server.js y un cliente prisma

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3000;
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+app.get('/', (req, res) => {
+  res.json({message: 'alive'});
+});
+
+app.listen(port, () => {
+  console.log(`Listening to requests on port ${port}`);
+});
