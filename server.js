02753c677a6660c0255ee36c9e63be6784d4cf8e
cesarmezab <99105052+CesarMezaB@users.noreply.github.com>
2022-05-13 23:40:59 -0700

    Agregando Endpoin POST users que crea un nuevo usuario

--- server.js
+++ server.js
@@ -21,6 +21,19 @@ app.get('/users', async (req, res) => {
     res.json(user);
   });
 
+  app.post('/users', async (req, res) => {
+    const user = {
+      name: req.body.name,
+      email: req.body.email,
+      animal_Specie: req.body.animal_Specie,
+      commentary: req.body.commentary
+
+     };
+    const message = 'Usuario creado creado.';
+    await prisma.user.create({data: user});
+    return res.json({message});
+  });
+
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
 });
