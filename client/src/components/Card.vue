f35f92e8709b4189e18955a55ac0a93d554157ca
ALEnsGAMER <62356565+alensgamer@users.noreply.github.com>
2022-05-14 20:09:10 -0500

    Interfaz beta

--- client/src/components/Card.vue
+++ client/src/components/Card.vue
@@ -1,9 +1,9 @@
 <template>
   <div
     class="animal-card handle"
-    @dblclick="isFlipped = !isFlipped"
-    :class="{ flip: isFlipped }"
+    @mouseup="isFlipped =! isFlipped" :class="{ flip: isFlipped }"
     ref="animal-card"
+    
   >
     <div
       class="face front"
@@ -33,7 +33,7 @@
         </div>
       </div>
     </div>
-  </div>
+  
 </template>
 
 <script>
@@ -74,7 +74,7 @@ export default {
   margin: 7px auto;
   box-sizing: border-box;
   padding: 10px;
-  position: relative;
+  position: absolute;
 }
 
 .winner-dice span {
@@ -104,16 +104,32 @@ export default {
 
 .animal-card {
   position: relative;
-  transform-style: preserve-3d;
-  transition: transform 0.5s;
-  cursor: pointer;
-  font-family: "vcr_osd_monoregular";
-  width: 400px;
-  height: 600px;
+    transform-style: preserve-3d;
+    transition: transform 0.5s;
+    cursor: pointer;
+    font-family: "vcr_osd_monoregular";
+    width: 24%;
+    height: 64%;
   color: white;
   margin: 10px;
   border-radius: 10px;
   user-select: none;
+  
+}
+#card-1{
+  position: absolute;
+  left: 12%;
+  height: 53%;
+}
+#card-2{
+  position: absolute;
+  right: 12% ;
+  height: 53%;
+}
+#card-3{
+  position: absolute;
+  justify-content: center;
+  height: 53%;
 }
 
 .animal-card.flip {
@@ -121,8 +137,12 @@ export default {
   transition: transform 0.5s !important;
 }
 
+.animal-card:hover {
+  transform: scale(1.05);
+}
+
 .animal-card .face {
-  position: absolute;
+  position: relative;
   top: 0px;
   left: 0px;
   bottom: 0px;
@@ -136,6 +156,8 @@ export default {
   transform: rotateY(180deg);
 }
 
+
+
 .animal-card .face {
   border-radius: 10px;
 }
@@ -146,6 +168,7 @@ export default {
   -moz-box-shadow: inset 0px -100px 45px 5px #000000;
   -webkit-box-shadow: inset 0px -100px 45px 5px #000000;
   box-shadow: inset 0px -100px 45px 5px #000000;
+  height: -webkit-fill-available;
 }
 
 .animal-card .front {
@@ -156,7 +179,7 @@ export default {
 }
 
 .animal-card .front .capacities {
-  position: absolute;
+  position: relative;
   bottom: 0;
   width: 100%;
   height: 70px;
@@ -171,6 +194,10 @@ export default {
   background: #333333;
   background-size: cover;
   color: #ffffff;
+  position: absolute;
+  left: 12%;
+  height: 53%;
+  padding: -10%;
 }
 
 .animal-card .back .header {
@@ -181,13 +208,18 @@ export default {
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
+  
 }
 .animal-card .back .description {
-  padding: 30px;
+  padding: -10%;
+  height: 100%;
+  position: absolute;
+  justify-content: safe;
 }
 
+
 .animal-card .back .logo {
-  position: absolute;
+  position: relative;
   bottom: 10px;
   right: 10px;
   width: 100px;
@@ -198,7 +230,7 @@ export default {
 .animal-card h2 {
   text-transform: uppercase;
   font-size: 58px;
-  position: absolute;
+  position: relative;
   bottom: 145px;
   margin: 0;
   line-height: 0.9em;
@@ -212,11 +244,26 @@ export default {
 }
 
 .animal-card .front p {
-  position: absolute;
+  position: relative;
   left: 0px;
   bottom: 0px;
   padding: 20px;
   font-size: 1rem;
   text-shadow: 1px 1px 2px black;
 }
+
+
+
+.fondo{
+  position: absolute;
+  width: 100%;
+  z-index: -1;
+}
+.app{
+  display: flex;
+  padding-top: 10%;
+  justify-content: center;
+  width: 100%;
+}
+
 </style>
