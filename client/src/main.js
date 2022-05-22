e4595f818ace8d86cfc83cec2a19bb0c72796a63
Bruno Gómez <br1_gio@hotmail.com>
2022-05-22 14:23:36 -0500

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -14,12 +14,12 @@ require('./css/app.css')
  */    
     
     if(cambiofondo.path[1].scrollY==400){
-      document.getElementById("Fondovid").src="https://vod-progressive.akamaized.net/exp=1652775089~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3558%2F5%2F142790249%2F428992243.mp4~hmac=bfced0bf292fcf6baf86599e4dd1a039b78a084c8cd3ffad04a7bbf1716a4edd/vimeo-prod-skyfire-std-us/01/3558/5/142790249/428992243.mp4";
+      document.getElementById("Fondovid").src="https://oceanx.blob.core.windows.net/container/Jellyfish%20-%2010480.mp4";
     }
     
 
     if(cambiofondo.path[1].scrollY==230){
-        document.getElementById("Fondovid").src="https://vod-progressive.akamaized.net/exp=1652736633~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2293%2F26%2F661469865%2F3041266541.mp4~hmac=ecc2ae2574636141ba096a0ecab3a3adaa4d1e1d68c81e59096e43accbe07f6d/vimeo-prod-skyfire-std-us/01/2293/26/661469865/3041266541.mp4";
+        document.getElementById("Fondovid").src="https://oceanx.blob.core.windows.net/container/Jellyfish%20-%2010480.mp4";
     }
 
   })
